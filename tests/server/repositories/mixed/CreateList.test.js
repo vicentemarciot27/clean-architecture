cce88eba9987efa91483b9bfdd9d9ba6a817b343
vicentemarciot27 <129920185+vicentemarciot27@users.noreply.github.com>
2023-10-12 02:02:01 -0300

    Update CreateList.test.js

--- tests/server/repositories/mixed/CreateList.test.js
+++ tests/server/repositories/mixed/CreateList.test.js
@@ -13,7 +13,6 @@ describe("InMemoryNoteRepository", () => {
       author_id: "1234",
       title: "Sample Note",
       content: "This is a sample note.",
-      // ... other note properties
     };
 
     const createdNote = await repository.CreateNote(
@@ -21,14 +20,13 @@ describe("InMemoryNoteRepository", () => {
       noteData.author_id,
       noteData.title,
       noteData.content
-      // ... pass other required parameters
     );
     
     expect(createdNote.author).toBe(noteData.author);
     expect(createdNote.author_id).toBe(noteData.author_id);
     expect(createdNote.title).toBe(noteData.title);
     expect(createdNote.content).toBe(noteData.content);
-    // ... assert other note properties
+    
   });
 
   it("should list all stored notes", async () => {
@@ -45,5 +43,4 @@ describe("InMemoryNoteRepository", () => {
     expect(notes[1].title).toBe("Title 2");
   });
 
-  // ... similarly, create tests for other methods like FindNoteByID, EditNoteTitle, etc.
 });
